6adea4f102b5440a8932fb52209ca24728ebc4b7
Matt Heerspink <mheerspink75@gmail.com>
2019-12-07 18:04:29 -0600

    updated express_server.js

--- express_server.js
+++ express_server.js
@@ -1,10 +1,51 @@
+var quotes = [
+    {
+        id: 1,
+        quote: "The best is yet to come",
+        author: "Unknown",
+        year: 2000
+    },
+    {
+        id: 2,
+        quote: "This is a quote",
+        author: "First Last",
+        year: 1930
+    },
+    {
+        id: 3,
+        quote: "This is another quote",
+        author: "First2 Last2",
+        year: 1910
+    }
+];
+
+
 var express = require('express');
 var app = express();
 var port = 3000;
 
+
 app.get(`/`, function (request, response) {
     response.send('Hello, World!');
 });
+
+
+app.get('/quotes', function (req, res) {
+    console.log("Get a list of all quotes as json");
+    if (req.query.year) {
+        res.send("Return a list of quotes from the year: " + req.query.year);
+    }
+    else {
+        res.json(quotes);
+    }
+});
+
+
+app.get('/quotes/:id', function(req, res){
+    console.log("return quote with the ID: " + req.params.id);
+    res.send("Return quote with the ID: " + req.params.id);
+});
+
 
 app.listen(port, function () {
     console.log('Express app listening on port ' + port);
